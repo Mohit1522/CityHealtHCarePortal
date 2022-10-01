3979de7320ea15a58a821991fe39c6fb93165b26
Mohit <mohitchouhan707@gmail.com>
2022-10-01 13:21:19 +0530

    firts commit

---
+++ BackEnd/CityHealthCarePortal-BakcEnd/src/test/java/com/cdac/Realme1805ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Realme1805ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

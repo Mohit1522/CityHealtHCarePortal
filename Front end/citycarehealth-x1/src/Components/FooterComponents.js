e3fdc02107f268be75cae177e23824d2c8eace9a
Mohit <mohitchouhan707@gmail.com>
2022-10-02 07:47:32 +0530

    FooterComponent Modifyed

--- Front end/citycarehealth-x1/src/Components/FooterComponents.js
+++ Front end/citycarehealth-x1/src/Components/FooterComponents.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const FooterComponents = () => {
   return (
@@ -13,38 +14,38 @@ const FooterComponents = () => {
 					<h3>About</h3>
 					<p>We aim's to provide the details of medical services of city on our website</p>
 					<ul>
-						<li><a href="#facebook">facebook</a></li>
-						<li><a href="#Twitter">Twitter</a></li>
-						<li><a href="#Instagram">Instagram</a></li>
-						<li><a href="#Yotube">YouTube</a></li>
+						<li><a href="https://www.facebook.com/">facebook</a></li>
+						<li><a href="https://twitter.com/?lang=en-in">Twitter</a></li>
+						<li><a href="https://www.instagram.com/">Instagram</a></li>
+						<li><a href="https://www.youtube.com/?gl=IN">YouTube</a></li>
 					</ul>
 				</div>
 				<div className="col-md-3 col-sm-6 b">
 					<h5>Hospital Admin</h5>
 					<ul>
-						<li><a href="/login">Login</a></li>
-						<li><a href="/">Get track of bed-availability</a></li>
-						<li><a href="/">Get track of oxygen-availability</a></li>
-						<li><a href="/">Get the patient request</a></li>
-						<li><a href="/">Status of patient</a></li>
+						<li><Link to="/login">Login</Link></li>
+						<li><Link to="/admindashboard">Get track of bed-availability</Link></li>
+						<li><Link to="/admindashboard">Get track of oxygen-availability</Link></li>
+						<li><Link to="/admindashboard">Get the patient request</Link></li>
+						<li><Link to="/admindashboard">Status of patient</Link></li>
 					</ul>
 				</div>
 				<div className="col-md-3 col-sm-6">
 					<h5>User</h5>
 					<ul>
-						<li><a href="/">Register</a></li>
-						<li><a href="/">Get the Hospital Details</a></li>
-						<li><a href="/">Search bed availability</a></li>
-						<li><a href="/">Track your status</a></li>
+						<li><Link to="/userdashboard">Register</Link></li>
+						<li><Link to="/userdashboard">Get the Hospital Details</Link></li>
+						<li><Link to="/userdashboard">Search bed availability</Link></li>
+						<li><Link to="/userdashboard">Track your status</Link></li>
 					</ul>
 				</div>
 				<div className="col-md-3 col-sm-6">
 					<h5>Have a question?</h5>
 					<ul>
-						<li><a href="/"> 603, Race course road, Mumbai,
-								India</a></li>
-						<li><a href="/">+91 2292 3929 210</a></li>
-						<li><a href="/">info@cityhealthcare.com</a></li>
+						<li><Link to="/contactus"> 603, Race course road, Mumbai,
+								India</Link></li>
+						<li><Link to="/contactus">+91 2292 3929 210</Link></li>
+						<li><Link to="/contactus">info@cityhealthcare.com</Link></li>
 					</ul>
 				</div>
 			</div>
